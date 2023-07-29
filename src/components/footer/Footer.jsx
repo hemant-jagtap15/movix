eab06ab7ae0f6dbf1aefca0febc4b71413cc5a21
Hemant Jagtap <hemantjagtap2003@gmail.com>
2023-07-29 20:18:03 +0530

    movix files

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Welcome to Movix! We are dedicated to bringing you the latest updates, reviews, and news from the world of cinema. Join us on this cinematic journey and indulge in the magic of movies.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;

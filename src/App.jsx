c23f8c9011e412374088b80a83599c250aac9aa1
Hemant Jagtap <hemantjagtap2003@gmail.com>
2023-07-30 12:34:08 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ import Footer from "./components/footer/Footer";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/PageNotFound";
+
 import Home from './pages/home/home';
+import PageNotFound from './pages/404/pageNotFound';
 
 function App() {
   const dispatch = useDispatch()

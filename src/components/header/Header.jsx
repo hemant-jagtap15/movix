568b84fee418d2b60ac716d6d24556dfa845e7f2
Hemant Jagtap <hemantjagtap2003@gmail.com>
2023-07-30 12:50:40 +0530

    errorfixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 import "./style.scss";
 
-import logo from "../../assets/movix-logo.svg";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
     const [show, setShow] = useState("top");
